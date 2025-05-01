534acd1d65a7fe21a0fac4cbf7b36b6e565913cc
Andydiii <Andydiii0813@gmail.com>
2025-05-01 16:19:08 -0400

    basics of using reactquery to fetch data

--- react-query-demo/src/components/PostsRQ.js
+++ react-query-demo/src/components/PostsRQ.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const PostsRQ = () => {
-  return (
-    <div className='container'>PostsRQ Page</div>
-  )
-}
+  // useQuery hook returns a object contains many different fields including data, isLoading, isError, error....
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"], // this must be unique
+    queryFn: () => {
+      return axios.get("http://localhost:4000/posts");
+    }, // a function alwasy returns a promise
+  });
 
-export default PostsRQ
+  if (isLoading) {
+    return <div>Page is loading...</div>;
+  }
+
+  if (isError) {
+    return <div> {error.message} </div>;
+  }
+
+  console.log(data);
+
+  return (
+    // we fetch from /posts endpoint, => set key as ["posts"]
+    // fetch from /posts/1 => set key = ["posts", 1] or ["posts", post.id]
+    // fetch from /posts/2 => set key = ["posts", 2] or ["posts", post.id]
+    // /posts/1/comments => ["posts", post.id, "comments"]
+    <div className="post-list">
+      {data.data.map((post) => {
+        return (
+          <div className="post-item" key={post.id}>
+            <h3 className="post-title">{post.title}</h3>
+            <p className="post-body">{post.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PostsRQ;
