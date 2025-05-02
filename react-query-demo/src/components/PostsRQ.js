b85806c0dca389eed9fc07fb625b505cd39f78f2
Andydiii <Andydiii0813@gmail.com>
2025-05-02 11:43:15 -0400

    react query cache advantage

--- react-query-demo/src/components/PostsRQ.js
+++ react-query-demo/src/components/PostsRQ.js
@@ -2,14 +2,20 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+// react query store the data in cache for 5 minutes
+  // but data in cache might be outdated, data in json server might have been updated, in this case, the react query doing the refetch in the backgroud, if it is updated, reflect on UI
+  // isLoading is true when there is no cache for the data, but if there is, isLoading is false.
+  // isFetching is true when you are fetching or refetching. no matter if there is cache for data.
 const PostsRQ = () => {
   // useQuery hook returns a object contains many different fields including data, isLoading, isError, error....
-  const { data, isLoading, isError, error } = useQuery({
+  const { data, isLoading, isError, error, isFetching } = useQuery({
     queryKey: ["posts"], // this must be unique
     queryFn: () => {
       return axios.get("http://localhost:4000/posts");
     }, // a function alwasy returns a promise
   });
+
+  console.log({isLoading, isFetching});
 
   if (isLoading) {
     return <div>Page is loading...</div>;
@@ -19,7 +25,7 @@ const PostsRQ = () => {
     return <div> {error.message} </div>;
   }
 
-  console.log(data);
+  
 
   return (
     // we fetch from /posts endpoint, => set key as ["posts"]
