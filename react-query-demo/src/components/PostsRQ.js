70e3b2c36218d4b9efafdc8afb44f2b73b67bb59
Andydiii <Andydiii0813@gmail.com>
2025-05-09 16:50:08 -0400

    stale mode is completed. move on to polling

--- react-query-demo/src/components/PostsRQ.js
+++ react-query-demo/src/components/PostsRQ.js
@@ -10,8 +10,9 @@ import axios from "axios";
   // react query assumes data is updating all the time, so every time you go the other page and come back would trigger the background refetch again. This can be seen in network
 // stale mode in devtool:
   // stale mode stands for outdated data bc react query assumes its updated all the time
-  // if I am a software developer and I know db data is not gonna change often, then I can set it by staleTime myself
-  // as long as the mode is in fresh, it wont trigger refetch later on for the time we set. **
+  // so this caused background refetch from the endpoint is triggered all the time when we click this page.
+  // if I am a software developer and I know db data is not gonna change often, then I can configure it by staleTime myself
+  // as long as the mode is in fresh, it wont trigger refetch during time we set. **
 const PostsRQ = () => {
   // useQuery hook returns a object contains many different fields including data, isLoading, isError, error....
   const { data, isLoading, isError, error, isFetching } = useQuery({
@@ -19,8 +20,8 @@ const PostsRQ = () => {
     queryFn: () => {
       return axios.get("http://localhost:4000/posts");
     }, // a function alwasy returns a promise
-    // how long it is frech data then become stale(outdated), default to be 0
-    staleTime: 10000
+    // how long in miliseconds it is frech data then become stale(outdated), default to be 0
+    staleTime: 10000 // 10 seconds
   });
 
   console.log({isLoading, isFetching});
