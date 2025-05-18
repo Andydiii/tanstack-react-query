2b919cf66e60da8b3505d16e7a3242bf3a7902af
Andydiii <Andydiii0813@gmail.com>
2025-05-18 18:34:38 -0400

    pagination in RQ is done

--- react-query-demo/src/components/PaginatedQueries.js
+++ react-query-demo/src/components/PaginatedQueries.js
@@ -1,16 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
+import {useState} from "react";
+
+function fetchFruits(pageId) {
+    return axios.get(`http://localhost:4000/fruits?_page=${pageId}&_per_page=4`);
+}
 
 export default function PaginatedQueries() {
-    function fetchFruits() {
-        return axios.get("http://localhost:4000/fruits");
-    }
+
+    const [page, setPage] = useState(1);
 
     const { data, isLoading, isError, error } = useQuery({
-        queryKey: ["fruits"],
-        queryFn: fetchFruits
-    }) 
+        queryKey: ["fruits", page],
+        queryFn: () => fetchFruits(page),
+        placeholderData: keepPreviousData // prevent showing loading text in the screen by remains the current UI in the page
+    })
+
+    // console.log(data);
 
     if (isLoading) {
         return <h2>Page is loading...</h2>
@@ -22,15 +29,19 @@ export default function PaginatedQueries() {
 
     return (
         <div className="container">
-            {data?.data.map(item => {
+            {data?.data.data.map(item => {
                 return (
-                    <div className="fruit-label">
+                    <div className="fruit-label" key={item.id}>
                         {item.name}
                     </div>
                 )
             })}
+            <button onClick={() => {setPage(page - 1)}} disabled={page === 1? true : false}>
+                Prev Page
+            </button>
+            <button onClick={() => {setPage(page + 1)}} disabled={page === 5? true : false}>
+                Next Page
+            </button>
         </div>
     )
-
-
 }
